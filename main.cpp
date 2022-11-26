db73292ec8e3e1cc5deea81587bef5bae7e23686
@mperrett <matthieu.perrette@etu.utc.fr>
2022-11-26 14:22:42 +0100

    Ajout de De et des monuments dans la fonction cartesEditionClassique

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include "./src/Carte.h"
 #include "./src/Paquet.h"
+#include "./src/Carte.h"
 
 using namespace std;
 
 int main() {
 /*
+    //test de vecteurs
 	//vector<int> vect = { 1, 3 };
 	//cout << vect[1] << "\n";
+
+
+	//test des types d'énumération
 	try {
 		printCouleurs();
 		printTypes();
@@ -14,6 +19,9 @@ int main() {
 	catch (CarteException& e) {
 		cout << e.getInfo() << "\n";
 	}
+
+
+	//test des cartes
 	//Effet effet;
 	//cout << effet.getDescription() << "\n";
 	//Carte c1(effet, Couleur::bleu, 2, vect, "Champ de ble", Type::champ);
@@ -24,6 +32,8 @@ int main() {
 		cout << *n << "\n";
 	}
 
+
+	//test des paquets
     Paquet paquet;
     for (auto n : cartes) {
         paquet.ajouterCarte(n);
@@ -39,6 +49,20 @@ int main() {
     for (auto n : cartas) {
         cout << *n << "\n";
     }
+
+
+	//test du dé
+	//seed permettant de faire des résultats vraiment aléatoire (on peut pas le mettre dans lancerDe() j'ai tester)
+	srand((unsigned)time(NULL));
+
+	De de(1,6);
+	cout << de;
+	int random;
+	for (size_t i = 0; i < 6; i++) {
+		random = de.lancerDe();
+		cout << random << "\n";
+	}
+
 
     for (auto n : cartes) {
         delete n;
