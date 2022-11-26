53c8d12cb701f80c2d899b1d9d9edde68aed1ea7
sesinian <antoine.sesini@etu.utc.fr>
2022-11-26 19:48:53 +0100

    Structure de départ de la classe Plateau

---
+++ src/Plateau.h
@@ -0,0 +1,56 @@
+//
+// Created by Antoine Sesini on 26/11/2022.
+//
+#pragma once
+#ifndef MINIVILLES_PLATEAU_H
+#define MINIVILLES_PLATEAU_H
+
+#include "Paquet.h"
+
+//********************Structures et variables nécessaires*****************//
+class PlateauException {
+private:
+    string info;
+public:
+    PlateauException(const string& i) :info(i) {}
+    string getInfo() const { return info; }
+};
+//********************Structures et variables nécessaires*****************//
+
+
+//****************class Plateau*******************//
+class Plateau {
+private :
+    vector<Paquet> cartes;
+public :
+    //Constructeur avec dispatch normal : on identifie les cartes et on les pointe 6 fois dans chaque Paquet (sauf exception)
+    Plateau(vector<Carte> cartesJeu); //cartesJeu=toutes les cartes de l'édition ne nécessitant pas de pioche et de distribution spéciale
+    //Constructeur avec un dispatch anormal : on utilise la pioche pour faire des tirages
+    Plateau(vector<Carte> cartesJeu, Pioche p);
+    Plateau(const Plateau&) = delete;
+    Plateau operator=(const Plateau&) = delete;
+    ~Plateau();
+
+
+
+
+    //Espace de définition des get et autres méthodes d'utilisation//
+    Paquet& getPaquet(string nom);
+    vector<Paquet> getPaquets();
+    vector<Paquet> getPaquetsNonVides();
+    unsigned int getNbPaquets();
+    Carte* retirerCarte(string nom);
+    Carte* ajouterCarte(Carte*){}
+    //Espace de définition des get et autres méthodes d'utilisation//
+
+
+};
+//****************class Plateau*******************//
+
+
+//****************Fonctions supplémentaires******************//
+ostream& operator<<(ostream& f, const Plateau& p);
+//****************Fonctions supplémentaires******************//
+
+
+#endif //MINIVILLES_PLATEAU_H
