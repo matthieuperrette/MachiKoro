e349368eb153fc0b6f2b5c951e693cc3c53aec02
Mathieu Bousquet <mathieu.bousquet@etu.utc.fr>
2022-11-26 01:13:15 +0000

    Debut code Joueur.h

--- src/Joueur.h
+++ src/Joueur.h
@@ -1,1 +1,27 @@
+#ifndef JOUEUR_H
+#define JOUEUR_H
+
+#include<string>
+#include "Paquet.h"
+
+class Joueur {
+
+private:
+    Paquet cartes;
+    string pseudo;
+    unsigned int money;
+    int des;
+    Joueur(const Joueur&) = delete;
+    Joueur& operator=(const Joueur&) = delete;
+
+public:
+    Joueur(string p): cartes( ), pseudo(p), money(), des(1){}
+    void afficher(std::ostream&) const override;
+    unsigned int getMoney() const { return money; }
+    string getPseudo() const { return pseudo; }
+    unsigned int getNbDes() const { return des; }
+    unsigned int getNbCarte() const { return cartes.getNbCarte(); }
+};
+
+#endif
 
