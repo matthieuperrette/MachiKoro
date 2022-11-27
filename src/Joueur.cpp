84b802b2fcb4e0f57f1780ab881f2bb188aa8703
Mathieu <mathieubousquet@etu.utc.fr>
2022-11-27 01:51:34 +0100

    Suite Code Classe Joueur

--- src/Joueur.cpp
+++ src/Joueur.cpp
@@ -1,1 +1,15 @@
+#include "Joueur.h"
+
+//****************Fonctions supplémentaires******************//
+ostream& operator<<(ostream& f, const Joueur& j) {
+    f << "/**********Affichage Joueur**********/\n";
+    f << "Pseudonyme: " << j.getPseudo() << "\n";
+    f << "Est-ce une IA? (0 pour non, 1 pour oui): " << j.getIa() << "\n";
+    f << "Paquet: " << j.getPaquet() << "\n";
+    f << "Argent disponible: " << j.getMoney() << "\n";
+    f <<"Nombre de dés jouables: "<< j.getNbDes()<<"\n";
+    f << "/**********Joueur Affiche**********/\n";
+    return f;
+}
+//****************Fonctions supplémentaires******************//
 
--- src/Joueur.h
+++ src/Joueur.h
@@ -1,1 +1,43 @@
+#ifndef JOUEUR_H
+#define JOUEUR_H
+
+#include<string>
+#include "Paquet.h"
+
+//********************Structures et variables nécessaires*****************//
+class JoueurException {
+private:
+    string info;
+public:
+    JoueurException(const string& i) :info(i) {}
+    string getInfo() const { return info; }
+};
+//********************Structures et variables nécessaires*****************//
+
+//****************class Paquet*******************//
+class Joueur {
+private:
+    string pseudo;
+    bool ia;
+    Paquet cartes;
+    unsigned int money;
+    unsigned int des;
+    
+public :
+    Joueur(string p, unsigned int i=0, const Paquet& c, unsigned int d=1): pseudo(p), ia(i), cartes(c), money(0), des(d){}
+    ~Joueur() = default;
+     string getPseudo() const { return pseudo; }
+     bool getIa() const { return ia; }
+     Paquet& getCarte() const { return cartes; }
+     unsigned int getMoney() const { return money; }
+     unsigned int getNbDes() const { return des; }
+};
+//****************class Joueur*******************//
+
+
+//****************Fonctions supplémentaires******************//
+ostream& operator<<(ostream& f, const Paquet& p); //Fonction d'affichage d'un paquet
+//****************Fonctions supplémentaires******************//
+
+#endif
 
