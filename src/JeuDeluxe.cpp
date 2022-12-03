7e6434bfbfe26f38fbf52b30b70f96e853581fc3
sesinian <antoine.sesini@etu.utc.fr>
2022-12-03 17:18:14 +0100

    Classe Jeu héritées : mise en place de la structure pour les 3 classes héritées

---
+++ src/JeuDeluxe.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Antoine Sesini on 03/12/2022.
+//
+#include "JeuDeluxe.h"
+#include "Jeu.h"
+
+//---Structures et variables necessaires---//
+//---Structures et variables necessaires---//
+
+
+//------------Classe JeuDeluxe----------//
+//------------Classe JeuDeluxe----------//
+
+//---------Fonctions supplementaires-------//
+//---------Fonctions supplementaires-------//
---
+++ src/JeuMarina.h
@@ -0,0 +1,22 @@
+//
+// Created by Antoine Sesini on 03/12/2022.
+//
+#pragma once
+#ifndef MINIVILLES_JEUMARINA_H
+#define MINIVILLES_JEUMARINA_H
+
+
+//---Structures et variables necessaires---//
+//---Structures et variables necessaires---//
+
+
+//------------Classe JeuMarina----------//
+class JeuMarina {
+
+};
+//------------Classe JeuMarina----------//
+
+//---------Fonctions supplementaires-------//
+//---------Fonctions supplementaires-------//
+
+#endif //MINIVILLES_JEUMARINA_H
