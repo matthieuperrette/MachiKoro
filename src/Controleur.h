1e4a9b575e9c91380fe751d36ff06c9cd4343510
sesinian <antoine.sesini@etu.utc.fr>
2022-12-08 18:10:40 +0100

    Version pré-dev de Controleur

---
+++ src/Controleur.h
@@ -0,0 +1,34 @@
+//
+// Created by Antoine Sesini on 08/12/2022.
+//
+#pragma once
+#ifndef MINIVILLES_CONTROLEUR_H
+#define MINIVILLES_CONTROLEUR_H
+#include "Jeu.h"
+#include "JeuClassique.h"
+#include "JeuMarina.h"
+#include "JeuGreenValley.h"
+#include "JeuDeluxe.h"
+
+//Structures et classes necessaires à la classe principale//
+//Structures et classes necessaires à la classe principale//
+
+class Controleur {
+private :
+    //attributs//
+    //attributs//
+
+    //constructeurs et destructeurs//
+    //constructeurs et destructeurs//
+
+    //autres methodes et structures necessaires//
+    //autres methodes et structures necessaires//
+public :
+    //methodes publiques//
+    //methodes publiques//
+};
+
+//Autres fonctions non membres//
+//Autres fonctions non membres//
+
+#endif //MINIVILLES_CONTROLEUR_H
