96fb6cb32d7eb8f50a3035e568eaef865db1d952
@mperrett <matthieu.perrette@etu.utc.fr>
2022-12-12 20:39:34 +0100

    Ajout de JeuGreenValley et quelques petites retouches

--- src/JeuDeluxe.h
+++ src/JeuDeluxe.h
@@ -28,9 +28,8 @@ private:
         cartes=fonctions::cartesEditionClassique();
         vector<Carte*> cartesGreenValley=fonctions::cartesEditionGreenValley();
         cartes.insert(cartes.end(),cartesGreenValley.begin(),cartesGreenValley.end());
-        // NE PAS OUBLIER D'ENLEVER LES COMMENTAIRES A L'AJOUT DE JEUMARINA
-        //vector<Carte*> cartesMarina=fonctions::cartesEditionMarina();
-        //cartes.insert(cartes.end(),cartesMarina.begin(),cartesMarina.end());
+        vector<Carte*> cartesMarina=fonctions::cartesEditionMarina();
+        cartes.insert(cartes.end(),cartesMarina.begin(),cartesMarina.end());
 
         // CREATION DE LA PIOCHE ET DU PLATEAU
         pioche = new Pioche(cartes,nbJoueurs);
@@ -42,25 +41,25 @@ private:
         Carte* carteDepart1= *(find_if(cartes.begin(),cartes.end(), [&nomCarteDepart1](Carte* c){return c->getNom()==nomCarteDepart1;}));
         string nomCarteDepart2="Boulangerie";
         Carte* carteDepart2=*(find_if(cartes.begin(),cartes.end(), [&nomCarteDepart2](Carte* c){return c->getNom()==nomCarteDepart2;}));
-        // NE PAS OUBLIER D'ENLEVER LES COMMENTAIRES A L'AJOUT DE CARTESEDITIONMARINA
-        //string nomCarteDepart3="Hotel de ville";
-        //Carte* carteDepart3= *(find_if(cartes.begin(),cartes.end(), [&nomCarteDepart3](Carte* c){return c->getNom()==nomCarteDepart3;}));
-        //ATTENTION RAJOUTER (|| !carteDepart3) DANS LA CONDITION DU IF SUIVANT A L'AJOUT DE CARTESEDITIONMARINA
-        if (!carteDepart1 || !carteDepart2) {throw JeuException("Attention ! les cartes de départ n'ont pu être sélectionnées");}
+        string nomCarteDepart3="Hotel de Ville";
+        Carte* carteDepart3= *(find_if(cartes.begin(),cartes.end(), [&nomCarteDepart3](Carte* c){return c->getNom()==nomCarteDepart3;}));
+        if (!carteDepart1 || !carteDepart2 || !carteDepart3) {throw JeuException("Attention ! les cartes de départ n'ont pu être sélectionnées");}
         vector<Joueur*> playerList=getJoueursList();
         for (auto j:playerList)
         {
             j->ajouterCarte(carteDepart1);
             j->ajouterCarte(carteDepart2);
-            // NE PAS OUBLIER D'ENLEVER LES COMMENTAIRES A L'AJOUT DE CARTESEDITIONMARINA
-            //j->ajouterCarte(carteDepart3);
+            j->ajouterCarte(carteDepart3);
         }
     }
 
-    ~JeuDeluxe(){
+    ~JeuDeluxe() {
         vector<Carte*>::iterator it;
-        for(it=cartes.begin(); it!=cartes.end();it++) //Destruction des cartes
-            delete *it;
+        for (it = cartes.begin(); it != cartes.end(); it++) //Destruction des cartes et des effets
+        {
+            delete (*it)->getEffet();
+            delete* it;
+        } 
         delete plateau;
         delete pioche;
     }
